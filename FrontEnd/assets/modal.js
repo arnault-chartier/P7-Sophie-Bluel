b9f21b4ecaba66d3c5c5820da3b602c1ddd0fa31
Arnault Chartier <arnault.chartier@gmail.com>
2024-08-03 17:28:19 +0200

    ensemble du projet fonctionnel

--- FrontEnd/assets/modal.js
+++ FrontEnd/assets/modal.js
@@ -21,6 +21,64 @@ const workCategorySelect = document.getElementById("work-category");
 const addPictureButton = document.getElementById("add-picture_button");
 const validateButton = document.getElementById("validate_button");
 
+// Event listener sur le bouton "modifier"
+modifyButton.addEventListener("click", () => {
+    // Ouverture de la modale
+    openModal();
+});
+
+// Event listener sur le bouton "fermer"
+closeButton.addEventListener("click", () => {
+    // Fermeture de la modale
+    closeModal()
+});
+
+// Event listener pour clic en dehors de la modale
+modal.addEventListener("click", function (event) {
+    if (event.target === modal) {
+        closeModal();
+    }
+});
+
+// Event listener sur le bouton de retour
+backButton.addEventListener("click", () => {
+    // Fermeture de la modale
+    closeModal();
+    // Ouverture de la modale
+    openModal();
+});
+
+// Event listener sur le bouton "Ajouter une photo"
+addPictureButton.addEventListener("click", () => {
+    // Affichage de la modale "Ajout photo"
+    displayAddWorkModal();
+});
+
+// Event listener sur le bouton d'upload de l'image pour afficher la prévisualisation
+addWorkButton.addEventListener("change", displayWorkPreview);
+
+// Event listeners sur les champs pour vérifier les changements
+addPictureButton.addEventListener("change", checkFormCompletion);
+workTitleInput.addEventListener("input", checkFormCompletion);
+workCategorySelect.addEventListener("change", checkFormCompletion);
+
+// Event listener sur le bouton "Valider" pour envoi du formulaire à l'API
+validateButton.addEventListener("click", async () => {
+    const formData = new FormData();
+    formData.append("image", addWorkButton.files[0]);
+    formData.append("title", workTitleInput.value);
+    formData.append("category", workCategorySelect.value);
+    const response = await fetch("http://localhost:5678/api/works", {
+        method: "POST",
+        headers: { "Authorization": `Bearer ${token}` },
+        body: formData,
+    });
+    // Si la réponse de l'API est positive (projet créé)
+    if (response.status === 201) {
+        closeModal()
+    }
+})
+
 // Fonction de réinitialisation de la galerie
 function resetGallery() {
     galleryElement.innerHTML = "";
@@ -29,6 +87,22 @@ function resetGallery() {
 // Fonction de réinitialisation du formulaire
 function resetForm() {
     addWorkForm.reset();
+}
+
+// Fonction de réinitialisation de la prévisualisation
+function resetPreview() {
+    workPreviewElement.innerHTML = "";
+}
+
+// Fonction de réinitialisation des catégories
+function resetCategories() {
+    workCategorySelect.innerHTML = "";
+}
+
+// Fonction de réinitialisation du bouton "Valider"
+function resetValidateButton() {
+    validateButton.disabled = true;
+    validateButton.classList.add("greyed");
 }
 
 // Fonction d'ouverture de la modale
@@ -41,45 +115,21 @@ function openModal() {
 
 // Fonction de fermeture de la modale
 function closeModal() {
-    // Réinitialisation de la gallerie
+    // Réinitialisation de la galerie
     resetGallery();
     // Réinitialisation du formulaire
     resetForm();
     // Réinitialisation de la preview
     resetPreview();
+    // Réinitialisation des catégories
+    resetCategories();
+    // Réinitialisation du bouton "Valider"
+    resetValidateButton();
     // Fermerture de la modale
     modal.close();
     // Affichage des travaux sur la page
     displayWorks();
 }
-
-// Event listener sur le bouton "fermer"
-closeButton.addEventListener("click", () => {
-    // Fermeture de la modale
-    closeModal()
-});
-
-// Event listener pour clic en dehors de la modale
-modal.addEventListener("click", function (event) {
-    if (event.target === modal) {
-        // Fermeture de la modale
-        closeModal()
-    }
-});
-
-// Event listener sur le bouton "modifier"
-modifyButton.addEventListener("click", () => {
-    // Ouverture de la modale
-    openModal();
-});
-
-// Event listener sur le bouton de retour
-backButton.addEventListener("click", () => {
-    // Fermeture de la modale
-    closeModal();
-    // Ouverture de la modale
-    openModal();
-});
 
 // Fonction de mise en page de la modale "Galerie photo"
 function displayGalleryModal() {
@@ -142,40 +192,6 @@ async function displayModalWorks() {
     }
 }
 
-// Event listener sur le bouton "Ajouter une photo"
-addPictureButton.addEventListener("click", () => {
-    // Affichage de la modale "Ajout photo"
-    displayAddWorkModal();
-});
-
-// Fonction de réinitialisation des catégories
-function resetCategories() {
-    workCategorySelect.innerHTML = "";
-}
-
-// Fonction d'affichage des <option> de catégories pour le champs <select>
-async function displayWorkCategory() {
-    // Récupération de la constante "categories" par appel de la fonction "getCategories"
-    const categories = await getCategories();
-    // Réinitialisation des catégories
-    resetCategories();
-    // Création de l'option par défaut
-    const defaultWorkCategoryOption = document.createElement("option");
-    defaultWorkCategoryOption.value = "default";
-    defaultWorkCategoryOption.innerText = "-- Sélectionnez une catégorie --"
-    // Rattachement de la balise au parent
-    workCategorySelect.appendChild(defaultWorkCategoryOption);
-    // Boucle qui permet de générer les options de catégories
-    for (const category of categories) {
-        // Création de la balise <option>
-        const workCategoryOption = document.createElement("option");
-        workCategoryOption.value = category.id;
-        workCategoryOption.innerText = category.name;
-        // Rattachement de la balise au parent
-        workCategorySelect.appendChild(workCategoryOption);
-    }
-}
-
 // Fonction de mise en page de la modale "Ajout photo"
 function displayAddWorkModal() {
     // Affichage du bouton de retour
@@ -200,12 +216,25 @@ function displayAddWorkModal() {
     displayWorkCategory();
 }
 
-// Event listener sur le bouton d'upload de l'image pour afficher la prévisualisation
-addWorkButton.addEventListener("change", displayWorkPreview);
-
-// Fonction de réinitialisation de la prévisualisation
-function resetPreview() {
-    workPreviewElement.innerHTML = "";
+// Fonction d'affichage des <option> de catégories pour le champs <select>
+async function displayWorkCategory() {
+    // Récupération de la constante "categories" par appel de la fonction "getCategories"
+    const categories = await getCategories();
+    // Création de l'option par défaut
+    const defaultWorkCategoryOption = document.createElement("option");
+    defaultWorkCategoryOption.value = "default";
+    defaultWorkCategoryOption.innerText = "-- Sélectionnez une catégorie --"
+    // Rattachement de la balise au parent
+    workCategorySelect.appendChild(defaultWorkCategoryOption);
+    // Boucle qui permet de générer les options de catégories
+    for (const category of categories) {
+        // Création de la balise <option>
+        const workCategoryOption = document.createElement("option");
+        workCategoryOption.value = category.id;
+        workCategoryOption.innerText = category.name;
+        // Rattachement de la balise au parent
+        workCategorySelect.appendChild(workCategoryOption);
+    }
 }
 
 // Fonction d'affichage de la prévisualisation de l'image sélectionnée
@@ -222,19 +251,16 @@ function displayWorkPreview() {
     workPreviewElement.appendChild(workPreview);
 }
 
-// Event listener sur le bouton "Valider" pour envoi du formulaire à l'API
-validateButton.addEventListener("click", async () => {
-    const formData = new FormData();
-    formData.append("image", addWorkButton.files[0]);
-    formData.append("title", workTitleInput.value);
-    formData.append("category", workCategorySelect.value);
-    const response = await fetch("http://localhost:5678/api/works", {
-        method: "POST",
-        headers: { "Authorization": `Bearer ${token}` },
-        body: formData,
-    });
-    // Si la réponse de l'API est positive (projet créé)
-    if (response.status === 201) {
-        closeModal()
+// Fonction d'activation/désactivation du bouton "Valider"
+function checkFormCompletion() {
+    // Vérifie si les trois champs sont remplis
+    if (addWorkButton.files.length > 0 && workTitleInput.value.trim() !== "" && workCategorySelect.value !== "default") {
+        // Active le bouton
+        validateButton.disabled = false;
+        validateButton.classList.remove("greyed");
+    } else {
+        // Désactive le bouton
+        validateButton.disabled = true;
+        validateButton.classList.add("greyed");
     }
-})
+}
