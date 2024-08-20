907e5272d908c7150c89e5e61ca5746c69f377f2
Arnault Chartier <arnault.chartier@gmail.com>
2024-08-20 23:18:46 +0200

    commit post S7 - correction validation formulaire envoi projet + amélioration code

--- FrontEnd/assets/modal.js
+++ FrontEnd/assets/modal.js
@@ -1,66 +1,45 @@
 // Importation de fonctions
-import { getWorks } from "./script.js";
-import { getCategories } from "./script.js";
-import { displayWorks } from "./script.js";
+import { getWorks, getCategories, displayWorks } from "./script.js";
 
 // Constantes générales
 const token = window.localStorage.getItem("token");
-const modifyButton = document.getElementById("modify-button");
 const modal = document.getElementById("modal");
 const galleryModalElements = document.querySelectorAll(".gallery_modal");
 const addWorkModalElements = document.querySelectorAll(".add-work_modal");
-const backButton = document.getElementById("back-button");
-const closeButton = document.getElementById("close-button");
 const galleryElement = document.getElementById("gallery_element");
 const addWorkForm = document.getElementById("add-work_form");
-const addWorkElement = document.getElementById("add-work_element");
 const addWorkButton = document.getElementById("work-upload_button");
 const workPreviewElement = document.getElementById("work-preview_element");
 const workTitleInput = document.getElementById("work-title");
 const workCategorySelect = document.getElementById("work-category");
-const addPictureButton = document.getElementById("add-picture_button");
 const validateButton = document.getElementById("validate_button");
 const errorMessage = document.getElementById("error-message");
 
 // Event listener sur le bouton "modifier"
-modifyButton.addEventListener("click", () => {
-    // Ouverture de la modale
-    openModal();
-});
+document.getElementById("modify-button").addEventListener("click", openModal);
 
 // Event listener sur le bouton "fermer"
-closeButton.addEventListener("click", () => {
-    // Fermeture de la modale
-    closeModal();
-});
+document.getElementById("close-button").addEventListener("click", closeModal);
 
 // Event listener pour clic en dehors de la modale
 modal.addEventListener("click", function (event) {
-    if (event.target === modal) {
-        // Fermeture de la modale
-        closeModal();
-    }
+    if (event.target === modal) closeModal();
 });
 
 // Event listener sur le bouton de retour
-backButton.addEventListener("click", () => {
-    // Fermeture de la modale
+document.getElementById("back-button").addEventListener("click", () => {
     closeModal();
-    // Ouverture de la modale
     openModal();
 });
 
 // Event listener sur le bouton "Ajouter une photo"
-addPictureButton.addEventListener("click", () => {
-    // Affichage de la modale "Ajout photo"
-    displayAddWorkModal();
-});
+document.getElementById("add-picture_button").addEventListener("click", displayAddWorkModal);
 
 // Event listener sur le bouton d'upload de l'image pour afficher la prévisualisation
 addWorkButton.addEventListener("change", displayWorkPreview);
 
 // Event listeners sur les champs pour vérifier les changements
-addPictureButton.addEventListener("change", checkFormCompletion);
+addWorkButton.addEventListener("change", checkFormCompletion);
 workTitleInput.addEventListener("input", checkFormCompletion);
 workCategorySelect.addEventListener("change", checkFormCompletion);
 
@@ -70,47 +49,35 @@ validateButton.addEventListener("click", async () => {
     formData.append("image", addWorkButton.files[0]);
     formData.append("title", workTitleInput.value);
     formData.append("category", workCategorySelect.value);
+
     const response = await fetch("http://localhost:5678/api/works", {
         method: "POST",
         headers: { "Authorization": `Bearer ${token}` },
         body: formData,
     });
+
     // Si la réponse de l'API est positive (projet créé)
     if (response.status === 201) {
         closeModal();
-    } else {
+    } else { // Sinon message d'erreur dans les autres cas
         errorMessage.style.display = "block";
     }
 })
 
-// Fonction de réinitialisation de la galerie
-function resetGallery() {
+// Fonction de réinitialisation des éléments de la modale
+function resetModalElements() {
+    // Réinitialisation de la galerie
     galleryElement.innerHTML = "";
-}
-
-// Fonction de réinitialisation du formulaire
-function resetForm() {
+    // Réinitialisation du formulaire
     addWorkForm.reset();
-}
-
-// Fonction de réinitialisation de la prévisualisation
-function resetPreview() {
+    // Réinitialisation de la prévisualisation
     workPreviewElement.innerHTML = "";
-}
-
-// Fonction de réinitialisation des catégories
-function resetCategories() {
+    // Réinitialisation des catégories
     workCategorySelect.innerHTML = "";
-}
-
-// Fonction de réinitialisation du bouton "Valider"
-function resetValidateButton() {
+    // Réinitialisation du bouton "Valider"
     validateButton.disabled = true;
     validateButton.classList.add("greyed");
-}
-
-// Fonction de réinitialisation du message d'erreur
-function resetErrorMessage() {
+    // Réinitialisation du message d'erreur
     errorMessage.style.display = "none";
 }
 
@@ -124,18 +91,8 @@ function openModal() {
 
 // Fonction de fermeture de la modale
 function closeModal() {
-    // Réinitialisation de la galerie
-    resetGallery();
-    // Réinitialisation du formulaire
-    resetForm();
-    // Réinitialisation de la preview
-    resetPreview();
-    // Réinitialisation des catégories
-    resetCategories();
-    // Réinitialisation du bouton "Valider"
-    resetValidateButton();
-    // Réinitialisation du message d'erreur
-    resetErrorMessage();
+    // Réinitialisation des éléments de la modale
+    resetModalElements();
     // Fermerture de la modale
     modal.close();
     // Affichage des travaux sur la page
@@ -145,15 +102,11 @@ function closeModal() {
 // Fonction de mise en page de la modale "Galerie photo"
 function displayGalleryModal() {
     // Masquage du bouton de retour
-    backButton.style.visibility = "hidden";
+    document.getElementById("back-button").style.visibility = "hidden";
     // Affichage des éléments de la modale "Galerie photo"
-    galleryModalElements.forEach(function (galleryModalElement) {
-        galleryModalElement.style.display = "block";
-    });
+    galleryModalElements.forEach(element => element.style.display = "block");
     // Masquage des éléments de la modale "Ajout photo"
-    addWorkModalElements.forEach(function (addWorkModalElement) {
-        addWorkModalElement.style.display = "none";
-    });
+    addWorkModalElements.forEach(element => element.style.display = "none");
     // Affichage de la galerie
     galleryElement.style.display = "grid";
     // Masquage du formulaire
@@ -166,29 +119,26 @@ function displayGalleryModal() {
 async function displayModalWorks() {
     // Récupération de la constante "works" par appel de la fonction "getWorks"
     const works = await getWorks();
+    // Initialisation de la chaîne HTML pour les projets
+    let worksHTML = "";
     // Boucle qui permet de générer les projets
     for (const work of works) {
-        // Création de la balise <figure> dédiée à un projet
-        const workElement = document.createElement("figure");
-        // Création de la balise <img> pour la photo
-        const workPicture = document.createElement("img");
-        workPicture.classList.add("work-picture");
-        workPicture.src = work.imageUrl;
-        workPicture.alt = work.title;
-        // Création de la balise <img> pour le bouton "supprimer"
-        const deleteButton = document.createElement("img");
-        deleteButton.id = work.id;
-        deleteButton.classList.add("delete-button");
-        deleteButton.src = "./assets/icons/trash.svg";
-        deleteButton.alt = "icône de suppression de l'image";
-        // Rattachement des balises aux parents
-        galleryElement.appendChild(workElement);
-        workElement.appendChild(workPicture);
-        workElement.appendChild(deleteButton);
-        // Event listener sur les boutons "supprimer un projet"
+        // Insertion et concaténation de la chaîne HTML pour chaque itération
+        worksHTML += `
+            <figure>
+                <img class="work-picture" src="${work.imageUrl}" alt="${work.title}">
+                <img id="${work.id}" class="delete-button" src="./assets/icons/trash.svg" alt="icône de suppression de l'image">
+            </figure>
+        `;
+    }
+    // Injection du contenu HTML généré dans la galerie
+    galleryElement.innerHTML = worksHTML;
+
+    // Event listener sur les boutons "supprimer un projet"
+    galleryElement.querySelectorAll('.delete-button').forEach(deleteButton => {
         deleteButton.addEventListener("click", async () => {
             // Fetch pour supression d'un projet
-            const response = await fetch("http://localhost:5678/api/works/" + deleteButton.id, {
+            const response = await fetch(`http://localhost:5678/api/works/${deleteButton.id}`, {
                 method: "DELETE",
                 headers: { "Authorization": `Bearer ${token}` },
             });
@@ -198,31 +148,27 @@ async function displayModalWorks() {
                 closeModal();
                 // Ouverture de la modale
                 openModal();
-            } else {
+            } else { // Sinon message d'erreur dans les autres cas
                 errorMessage.style.display = "block";
             }
-        })
-    }
+        });
+    });
 }
 
 // Fonction de mise en page de la modale "Ajout photo"
 function displayAddWorkModal() {
     // Affichage du bouton de retour
-    backButton.style.visibility = "visible";
+    document.getElementById("back-button").style.visibility = "visible";
     // Masquage des éléments de la modale "Galerie photo"
-    galleryModalElements.forEach(function (galleryModalElement) {
-        galleryModalElement.style.display = "none";
-    });
+    galleryModalElements.forEach(element => element.style.display = "none");
     // Affichage des éléments de la modale "Ajout photo"
-    addWorkModalElements.forEach(function (addWorkModalElement) {
-        addWorkModalElement.style.display = "block";
-    });
+    addWorkModalElements.forEach(element => element.style.display = "block");
     // Masquage de la galerie
     galleryElement.style.display = "none";
     // Affichage du formulaire
     addWorkForm.style.display = "flex";
     // Affichage de la <div> d'upload
-    addWorkElement.style.display = "flex";
+    document.getElementById("add-work_element").style.display = "flex";
     // Masquage de la <div> de preview
     workPreviewElement.style.display = "none";
     // Affichage des <option> de catégories
@@ -233,33 +179,29 @@ function displayAddWorkModal() {
 async function displayWorkCategory() {
     // Récupération de la constante "categories" par appel de la fonction "getCategories"
     const categories = await getCategories();
-    // Création de l'option par défaut
-    const defaultWorkCategoryOption = document.createElement("option");
-    defaultWorkCategoryOption.value = "default";
-    defaultWorkCategoryOption.innerText = "-- Sélectionnez une catégorie --"
-    // Rattachement de la balise au parent
-    workCategorySelect.appendChild(defaultWorkCategoryOption);
+    // Initialisation de la chaîne HTML pour les options, avec option par défaut 
+    let optionsHTML = '<option value="default">-- Sélectionnez une catégorie --</option>';
     // Boucle qui permet de générer les options de catégories
     for (const category of categories) {
-        // Création de la balise <option>
-        const workCategoryOption = document.createElement("option");
-        workCategoryOption.value = category.id;
-        workCategoryOption.innerText = category.name;
-        // Rattachement de la balise au parent
-        workCategorySelect.appendChild(workCategoryOption);
+        // Insertion et concaténation de la chaîne HTML pour chaque itération
+        optionsHTML += `<option value="${category.id}">${category.name}</option>`;
     }
+    // Injection du contenu HTML généré dans le select
+    workCategorySelect.innerHTML = optionsHTML;
 }
 
 // Fonction d'affichage de la prévisualisation de l'image sélectionnée
 function displayWorkPreview() {
     // Masquage de la <div> d'upload
-    addWorkElement.style.display = "none";
+    document.getElementById("add-work_element").style.display = "none";
     // Affichage de la <div> de preview
     workPreviewElement.style.display = "flex";
     // Création de la balise <img> pour la preview
     const workPreview = document.createElement("img");
     // Récupération de l'image sélectionnée en tant que source pour la balise <img>
     workPreview.src = URL.createObjectURL(addWorkButton.files[0]);
+    // Réinitialisation de l'ancienne preview si elle existe
+    workPreviewElement.innerHTML = "";
     // Rattachement de l'élement au parent
     workPreviewElement.appendChild(workPreview);
 }
